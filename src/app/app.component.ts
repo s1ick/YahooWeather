ed52420b01bcaf2970d5ce894e09903c1d578346
Ivan Matveev <imatveev@biarum.com>
2025-02-05 10:56:09 +0300

    update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodayComponent } from './today/today.component';
+import { WeekComponent } from './week/week.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodayComponent, WeekComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
